ed78ae3adf9803dc2a8ae850f697e8da624bc5fb
THAMIZH <thamizharasan2373@gmail.com>
2021-07-09 12:36:16 +0530

    about images added

--- src/components/about/carouselDetrails.js
+++ src/components/about/carouselDetrails.js
@@ -1,15 +1,38 @@
-import oceanAcademy from "../../images/our_works/oa.png";
-import patchi from "../../images/our_works/patchi.jpg";
-import ELearning from "../../images/our_works/e-learning.jpg";
+import dream from "../../images/about_images/dream.jpg";
+import multiVendor from "../../images/about_images/muilt vendor.jpg";
+import complexTech from "../../images/about_images/complex tech.jpg";
+
+import buildMore from "../../images/about_images/build more.jpg";
+import reachGoals from "../../images/about_images/reach their goals.jpg";
+import sarveCustomers from "../../images/about_images/sarve their customers.jpg";
 
 export let carousels = [
   {
-    image: oceanAcademy,
-    title: "Ocean Academy",
+    image: sarveCustomers,
+    title: "Helping our customers to serve their customers",
   },
   {
-    image: patchi,
-    title: "PATCHI",
+    image: reachGoals,
+    title: "Helping our customers to reach their goals better and faster",
   },
-  { image: ELearning, title: "OA E-Learning" },
+  {
+    image: dream,
+    title: "Expertise that helps our Employees to live their dreams",
+  },
+  {
+    image: buildMore,
+    title: "Expertise that helps to build more experts in society",
+  },
+  {
+    image: multiVendor,
+    title: "Extensive experience in Multivendor Interoperability",
+  },
+  {
+    image: complexTech,
+    title: "Providing experience in advanced and complex technology projects",
+  },
+  // {
+  //   image: dream,
+  //   title: "Expertise in applying technology within financial constraints",
+  // },
 ];
